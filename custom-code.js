ed0e88951ebfe1509f32eb34d6420cb766f41a95
Dave Baskind <dave@picketstudio.com>
2024-01-18 11:50:00 +1100

    Fix bugs from previous commit and add quote stripping

--- custom-code.js
+++ custom-code.js
@@ -5,25 +5,26 @@ window._harvestPlatformConfig = {
 
 $(document).ready(function(){
   const { pathname, href } = window.location;
-  if (isPulseView(pathname)) setTimeout(main(pathname), 1000);
+  if (isPulseView(pathname)) setTimeout(main(pathname, href), 1000);
   
   window.addEventListener('locationchange', function () {
-    const newHref = window.location.href;
-    const newPathname = window.location.pathname;
-    if (isPulseView(newPathname) && newHref != href) setTimeout(main(newPathname), 1000);
+    const { pathname, href } = window.location;
+    if (isPulseView(pathname)) setTimeout(main(pathname, href), 1000);
   });
 });
 
-function main(pathname){
+function main(pathname, href){
   return function() {
     // Remove any existing timer buttons
+    const timer = $('.harvest-timer');
     $('.harvest-timer').remove();
     
-    const tasktitle = $(".slide-panel.open .title-wrapper h2").text();
-    const taskurl = window.location.href;
+    const title = $(".slide-panel.open .title-wrapper h2").text();
+    const tasktitle = title.replaceAll(/['"]/g,'');
+    const taskurl = href;
     
     const taskid = pathname.substring(pathname.lastIndexOf('/')+1);
-    const projectid = location.pathname.split('/')[2];
+    const projectid = pathname.split('/')[2];
     const projectname = $("#board-header .board-name").find(".ds-text-component").text();
     const actionsWrapper = $(".pulse_actions_wrapper");
     actionsWrapper.append("<div class='harvest-timer' id='harvest-timer-obj' style='height: 28px; width: 30px; text-align: center; padding-top: 6px; margin-left: 8px; color: #fff; background: #fff; border-radius: 4px; border: 2px solid #f58933;' data-item='{\"id\":\""+taskid+"\", \"name\": \""+tasktitle+"\"}' data-permalink='"+taskurl+"' data-group='{\"id\": \""+projectid+"\", \"name\": \""+projectname+"\" }' >");
